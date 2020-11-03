ba2e043d7b07a7a2602e32574e48256b5cee8733
jonquag <jon.quag@gmail.com>
2020-11-03 14:33:11 -0500

    Update 01
    First update.

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 const app = Vue.createApp({
     data() {
         return {
-            thing: "objects and stuff"
+            projects: [
+                    {   
+                        id: 001,
+                        title: "Card #1",
+                        thumb: "thumbnail",
+                        desc: "This is a description",
+                        area: 'a'
+                    },
+                    {
+                        id: 002,
+                        title: "Card #2",
+                        thumb: "thumbnail",
+                        desc: "This is a description",
+                        area: 'b' 
+                    },
+                    {
+                        id: 003,
+                        title: "Card #3",
+                        thumb: "thumbnail",
+                        desc: "This is a description",
+                        area: 'b' 
+                    },
+                    {
+                        id: 004,
+                        title: "Card #4",
+                        thumb: "thumbnail",
+                        desc: "This is a description",
+                        area: 'b' 
+                    },
+                ]
+            }
         }
-
-    },
 })
