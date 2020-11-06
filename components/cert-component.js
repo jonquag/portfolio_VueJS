8559e01d15d9ecbb1f247d6a2892effa1d6e062d
jonquag <jon.quag@gmail.com>
2020-11-06 10:36:07 -0500

    Added to Certifcation and Contact component

--- components/cert-component.js
+++ components/cert-component.js
@@ -1,10 +1,22 @@
 app.component('cert-component', {
     template:
     /*html*/
-    ``,
+    `
+        <img src="./assets/imgs/py4e-certificate.png" 
+            alt="Python Certificate"
+            class="cert__thumb">
+        <img src="./assets/imgs/wd4e-certificate.png" 
+            alt="Web Development Certificate"
+            class="cert__thumb">
+        <img src="./assets/imgs/JavaScript-Codecademy-Certificate.png" 
+            alt="JavaScript Certificate"
+            class="cert__thumb">
+        <img src="./assets/imgs/Udemy-JS-UWP.jpg" 
+            alt="JavaScript Understanding the Weird Parts Certificate"
+            class="cert__thumb">`,
     data() {
         return {
-
+            py4e: "./assets/imgs/py4e-certificate.png"
         }
     }
 
--- components/contact-component.js
+++ components/contact-component.js
@@ -1,7 +1,21 @@
 app.component('contact-component',{
     template:
     /*html*/
-    ``,
+    `
+    
+    <a href="mailto:jon.quag@gmail.com"
+                class="icon">
+        <img    src="./assets/imgs/mail.svg" 
+                alt="Mail icon"></a>
+    <a href="https://www.linkedin.com/in/jonathan-quagliariello-6372b5a6/"
+                class="icon">
+        <img    src="./assets/imgs/linkedin.svg" 
+                alt="Linkedin icon"></a>
+    <a href="https://github.com/jonquag"
+                class="icon">
+        <img    src="img/linkedin.svg" 
+                alt="Github icon"></a>
+    `,
     data() {
         return {
             
